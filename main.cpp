76ed1de42ceb7443fd19ece54ca017fd868a900f
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-06 11:17:22 +0300

    5

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 int main() {
     unsigned int value;
     std::cin >> value;
-    unsigned int newValue = value >> 6;
-    std::cout << std::bitset<32>(newValue);
-    std::cout << "\n" << newValue;
+    unsigned int maska = 1 << 31;
+    unsigned int n;
+    std::cin >> n;
+    maska = ~(maska>>(32-n-1));
+    std::cout <<std::bitset<32>(maska) << "\n";
+    std::cout << std::bitset<32>(maska&value);
     return 0;
 }
