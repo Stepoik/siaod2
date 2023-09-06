27a94a02a18893a24a920c245f3acc164ee4f45b
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-06 10:53:40 +0300

    rewrite to unsigned

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "bitset"
 
 int main() {
-    int value = 0x01F1;
-    int newValue = value|0b101000000000000010;
+    unsigned int value = 0x01F1;
+    unsigned int newValue = value|0b101000000000000010;
     std::cout << std::bitset<32>(newValue);
     return 0;
 }
