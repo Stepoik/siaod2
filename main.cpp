e508ac9f72a40d1514b0f80e1b1fb613f60bec05
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-06 10:57:34 +0300

    four

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     unsigned int value;
     std::cin >> value;
-    unsigned int newValue = value << 6;
+    unsigned int newValue = value >> 6;
     std::cout << std::bitset<32>(newValue);
     std::cout << "\n" << newValue;
     return 0;
