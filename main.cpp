452ade1aa68f1924e5cf8e5fcf866e76182f9651
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-07 12:25:24 +0300

    rewrite to bit operations

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@
 int main() {
     unsigned int value;
     std::cin >> value;
-    unsigned int maska = 0;
-    maska-=1;
-    maska-=(32+16+8);
+    unsigned int maska = ~0;
+    maska = maska&(~((~(1 << 3))<<2));
     std::cout << std::bitset<32>(maska) << "\n";
     unsigned int newValue = value&maska;
     std::cout << std::bitset<32>(newValue);
