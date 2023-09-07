0fe724c30a479e43dc9a02328f9a761f86ee9d23
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-07 12:31:21 +0300

    fixed branch

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include "bitset"
+#include "vector"
 
 int main() {
-    unsigned int value;
-    std::cin >> value;
-    unsigned int maska = 1 << 31;
-    unsigned int n;
-    std::cin >> n;
-    maska = ~(maska>>(32-n-1));
-    std::cout <<std::bitset<32>(maska) << "\n";
-    std::cout << std::bitset<32>(maska&value);
-    return 0;
+	int n;
+	std::cin >> n;
+	std::vector<unsigned int> values;
+	for (int i = 0; i < n; i++) {
+		unsigned int value;
+		std::cin >> value;
+		values.push_back(value);
+	}
+	unsigned char array = 0;
+	for (unsigned int i: values) {
+		array += 1 << (i);
+	}
+	int index = 0;
+	for (int i = 0; i < 8; i++) {
+		if (array%2 == 1){
+			values[index] = i;
+			index++;
+		}
+		array/=2;
+	}
+	for (unsigned int i: values) {
+		std::cout << i << "\n";
+	}
+	return 0;
 }
