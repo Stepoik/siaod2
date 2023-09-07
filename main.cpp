709b1fc8760bc3672c57c47d623bfc5600263e77
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-07 12:17:07 +0300

    first hw

--- main.cpp
+++ main.cpp
@@ -3,32 +3,9 @@
 #include "vector"
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::vector<unsigned int> values;
-    for (int i = 0; i < n; i++) {
-        unsigned int value;
-        std::cin >> value;
-        values.push_back(value);
-    }
-    unsigned char zero = 0;
-    std::vector<unsigned char> array = std::vector{zero, zero, zero, zero, zero, zero, zero, zero};
-    for (unsigned int i: values) {
-        int index = i / 8;
-        array[index] += 1 << (i % 8);
-    }
-    int index = 0;
-    for (int j = 0; j < array.size(); j++) {
-        for (int i = 0; i < 8; i++) {
-            if (array[j]%2 == 1){
-                values[index] = j*8+i;
-                index++;
-            }
-            array[j]/=2;
-        }
-    }
-    for (unsigned int i: values) {
-        std::cout << i << "\n";
-    }
+    unsigned int maska = (1<<17)+(1<<15)+(1<<1);
+	unsigned int value = 0xA512;
+	std::cout << std::bitset<32>(value) << "\n";
+	std::cout << std::bitset<32>(maska|value);
     return 0;
 }
