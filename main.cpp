b209075a50acbb8d32e23749ed43fc6599fb3b15
Stepoik <83645255+Stepoik@users.noreply.github.com>
2023-09-10 22:44:56 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "bitset"
 
 int main() {
-    unsigned int value;
-    std::cin >> value;
-    unsigned int maska = ~0;
-    maska = maska&(~((~(1 << 3))<<2));
-    std::cout << std::bitset<32>(maska) << "\n";
-    unsigned int newValue = value&maska;
-    std::cout << std::bitset<32>(newValue);
-    return 0;
+	unsigned int value;
+	std::cin >> value;
+	unsigned int maska = ~0;
+	maska = maska&(~((~(~0 << 3))<<2));
+	std::cout << std::bitset<32>(value) << "\n";
+	unsigned int newValue = value&maska;
+	std::cout << std::bitset<32>(newValue);
+	return 0;
 }
